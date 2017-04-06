0d63674af67ddf080ccba7d870e6d0733910a9db
cesiu <christopher@get2thecrossing.com>
2017-04-06 12:00:19 -0700

    Added sexpdata library

--- translate.py
+++ translate.py
@@ -2,6 +2,18 @@
 # CPE 530, Spring '17
 # Andrew Tran and Christopher Siu
 
+from sexpdata import loads as parse
+
+# LC = num
+#    | id
+#    | (λ (id) LC)
+#    | (LC LC)
+#    | (+ LC LC)
+#    | (* LC LC)
+#    | (ifleq0 LC LC lC)
+#    | (println LC)
+# Do we want to hardcode +, -, ifleq0, and println, or make them lambdas?
+
 def main():
     pass
 
