34da42c0374571f02499e6f9c7d687a49837ab5e
cesiu <christopher@get2thecrossing.com>
2017-04-06 10:08:31 -0700

    Initial commit.

---
+++ translate.py
@@ -0,0 +1,9 @@
+# Lambda Calculus -> Python Translator
+# CPE 530, Spring '17
+# Andrew Tran and Christopher Siu
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ translate_tests.py
@@ -0,0 +1,12 @@
+# Lambda Calculus -> Python Translator
+# CPE 530, Spring '17
+# Andrew Tran and Christopher Siu
+
+import unittest
+
+class TranslateTests(unittest.TestCase):
+    def test0_basic(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
