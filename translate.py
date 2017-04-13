611af0b8f23d594a534261976d47418c27d33fb6
Christopher <christopher@get2thecrossing.com>
2017-04-13 12:37:23 -0700

    Added wrapper print function that returns 0.

--- translate.py
+++ translate.py
@@ -16,14 +16,26 @@ from sexpdata import loads as parse
 #    | (println LC)
 # Do we want to hardcode +, -, ifleq0, and println, or make them lambdas?
 
+_print_wrapper = "def lprintln(string):\n"\
+                 "    print(string)\n"\
+                 "    return 0\n"
+
+_footer = "if __name__ == \"__main__\":\n"\
+          "    main()\n"
+
 def main():
+    global _print_wrapper
+    global _footer
+
     if len(sys.argv) != 2:
         print("Usage: python3 translate.py <file>", file = sys.stderr)
         sys.exit(1)
 
     with open(sys.argv[1], "r") as lamc_src,\
          open("ltp.out", "w") as py_out:
+        py_out.write(_print_wrapper)
         py_out.write("%s\n" % str(parse(lamc_src.read().strip())))
+        py_out.write(_footer)
 
 if __name__ == "__main__":
     main()
